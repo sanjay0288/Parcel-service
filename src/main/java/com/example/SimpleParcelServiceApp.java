ef8d67bbdac7a8b8c060ffe71b8786010c5c759f
sanjay0288 <87768165+sanjay0288@users.noreply.github.com>
2024-01-23 14:19:49 +0530

    Update SimpleParcelServiceApp.java

--- src/main/java/com/example/SimpleParcelServiceApp.java
+++ src/main/java/com/example/SimpleParcelServiceApp.java
@@ -1,12 +1,18 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class SimpleParcelServiceApp {
+public class SimpleParcelServiceAppWar extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SimpleParcelServiceApp.class);
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SimpleParcelServiceApp.class, args);
+        new SimpleParcelServiceAppWar()
+                .configure(new SpringApplicationBuilder(SimpleParcelServiceApp.class))
+                .run(args);
     }
 }
-
